5e5552e4a3714dc9bf5a8fe22d2c459909ea9846
Nathan Pham <nathanpham.me@gmail.com>
2021-01-29 23:56:08 +0000

    submit feature

--- public/js/index.js
+++ public/js/index.js
@@ -1,36 +1,19 @@
 const $ = (q) => document.querySelector(q)
 const app = $("#app")
+const input = $("input")
 const button = $("button")
 
-/*
-
-<div id="app">
-    <main>
-      <h1>ultimate-proxy</h1>
-      <input placeholder="example.com" />
-      <div class="app-options">
-        <div>
-          <a href="/help">
-            <i class="fas fa-question"></i>
-          </a>
-          <a href="https://github.com/nathan-pham/ultimate-proxy">
-            <i class="fab fa-github"></i>
-          </a>
-        </div>
-        <button>submit</button>
-      </div>
-    </main>
-	</div>
-  const $ = (q) => document.querySelector(q)
-
-const app = $(".app")
-const input = $(".form-password")
-const button = $(".form-button")
-
-button.addEventListener("click", () => {
+const submit = () => {
   app.innerHTML = 
   `
-  <iframe src="https://${ input.value }" sandbox="allow-scripts allow-forms allow-same-origin"></iframe>
+  <iframe src="${window.location}/fetch/${input.value}" sandbox="allow-scripts allow-forms allow-same-origin"></iframe> 
   `
+}
+
+input.addEventListener("keydown", e => {
+  if(e.key == "Enter") {
+    submit()
+  }
 })
-   */
+
+button.addEventListener("click", submit)
